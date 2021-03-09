22bcb5467f3da2ce206147d2440f77c9745762b4
Pierre Gaimard <pierre@gaimard-family.fr>
2021-03-09 16:12:01 +0100

    Add unit test

---
+++ tests/Unit/UnitTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UnitTest extends TestCase
+{
+    /**
+     * @group unit
+     */
+    public function testOk()
+    {
+        $this->assertEquals('a', 'a');
+    }
+}
