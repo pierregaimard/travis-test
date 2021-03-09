302606a180dbb1b2c92395f10f09e87283c62860
Pierre Gaimard <pierre@gaimard-family.fr>
2021-03-09 11:52:11 +0100

    Add first application test

---
+++ tests/Controller/TestControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use PHPUnit\Framework\TestCase;
+
+class TestControllerTest extends TestCase
+{
+    public function testSuccess()
+    {
+        $this->assertEquals(1,1);
+    }
+}
